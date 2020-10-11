15ad663f58340ffd321127bfa79605a6d850a042
Bashir Arslanaliev <bash.arslanaliev@gmail.com>
2020-10-11 23:37:55 +0300

    Initional commit

--- UnknownNumber/main.swift
+++ UnknownNumber/main.swift
@@ -1,11 +1,31 @@
-//
-//  main.swift
-//  UnknownNumber
-//
-//  Created by Башир on 11.10.2020.
-//
-
 import Foundation
 
-print("Hello, World!")
+let randomRange: UInt32 = 50
+let randomNumber = UInt8(arc4random_uniform(randomRange))
+var userInput: String?
+var count: Int = 0
 
+print("Задано случайное число от 0 до 50. Отгадайте его!")
+repeat {
+    print("Введите число:", terminator: " ")
+    userInput = readLine()
+    
+    if let input = UInt8(userInput!) {
+        if input <= randomRange && input >= 0 {
+            if input > randomNumber {
+                print("Нужно меньше")
+                count += 1
+            } else if input < randomNumber {
+                print("Нужно больше")
+                count += 1
+            } else {
+                print("Верно! Количество попыток: \(count)")
+                break
+            }
+        } else {
+            print("Введенное число выходит за рамки заданного диапазона!")
+        }
+    } else {
+        print("Это вообще не число!")
+    }
+} while true

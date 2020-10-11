6a39a3eab7d41d0375342e2884d91ec4d5d12453
Bashir Arslanaliev <bash.arslanaliev@gmail.com>
2020-10-12 00:27:52 +0300

    Оптимизация и небольшой рефакторинг

--- UnknownNumber/main.swift
+++ UnknownNumber/main.swift
@@ -4,6 +4,14 @@ let randomRange: UInt32 = 50
 let randomNumber = UInt8(arc4random_uniform(randomRange))
 var userInput: String?
 var count: Int = 0
+let message = [
+    "win" : "Верно! Количество попыток: ",
+    "more" : "Нужно больше",
+    "less" : "Нужно меньше",
+    "non-int" : "Это вообще не число!",
+]
+
+print("Debug. \(randomNumber)")
 
 print("Задано случайное число от 0 до 50. Отгадайте его!")
 repeat {
@@ -11,21 +19,18 @@ repeat {
     userInput = readLine()
     
     if let input = UInt8(userInput!) {
-        if input <= randomRange && input >= 0 {
-            if input > randomNumber {
-                print("Нужно меньше")
-                count += 1
-            } else if input < randomNumber {
-                print("Нужно больше")
-                count += 1
-            } else {
-                print("Верно! Количество попыток: \(count)")
-                break
-            }
+        count += 1
+        if input > randomNumber {
+            print(message["less"]!)
+            
+        } else if input < randomNumber {
+            print(message["more"]!)
+            
         } else {
-            print("Введенное число выходит за рамки заданного диапазона!")
+            print(message["win"]!, count)
+            break
         }
     } else {
-        print("Это вообще не число!")
+        print(message["non-int"]!)
     }
 } while true

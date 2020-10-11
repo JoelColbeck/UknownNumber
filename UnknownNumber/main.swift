85b18d2fe0ed529d220455a4fe4817cb08fac454
Bashir Arslanaliev <bash.arslanaliev@gmail.com>
2020-10-12 00:41:17 +0300

    Добавлена возможность начала новой игры без перезапуска приложения

--- UnknownNumber/main.swift
+++ UnknownNumber/main.swift
@@ -1,20 +1,23 @@
 import Foundation
 
 let randomRange: UInt32 = 50
-let randomNumber = UInt8(arc4random_uniform(randomRange))
+var randomNumber = UInt8(arc4random_uniform(randomRange))
 var userInput: String?
 var count: Int = 0
 let message = [
-    "win" : "Верно! Количество попыток: ",
+    "win" : "Верно! Количество попыток:",
     "more" : "Нужно больше",
     "less" : "Нужно меньше",
-    "non-int" : "Это вообще не число!",
+    "newGame" : "Хотите повторить? (+/-)",
+    "undefined" : "Не то вводите...",
+    "randomNumber" : "Задано случайное число от 0 до 50. Отгадайте его!",
 ]
 
 print("Debug. \(randomNumber)")
 
-print("Задано случайное число от 0 до 50. Отгадайте его!")
-repeat {
+print(message["randomNumber"]!)
+mainLoop: repeat {
+    
     print("Введите число:", terminator: " ")
     userInput = readLine()
     
@@ -28,9 +31,27 @@ repeat {
             
         } else {
             print(message["win"]!, count)
-            break
+            
+            // Новая игра
+            print(message["newGame"]!)
+            
+            repeat {
+                userInput = readLine()
+                
+                switch userInput {
+                case "+":
+                    print(message["randomNumber"]!)
+                    randomNumber = UInt8(arc4random_uniform(randomRange))
+                    count = 0
+                    continue mainLoop
+                case "-":
+                    break mainLoop
+                default:
+                    print(message["undefined"]!)
+                }
+            } while true
         }
     } else {
-        print(message["non-int"]!)
+        print(message["undefined"]!)
     }
 } while true
